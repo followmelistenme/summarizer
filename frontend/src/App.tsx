e9d1ff7739727d9e5fd497d3adbeef8694d87944
Konstantin Pritychenko <k.pritychenko@hh.ru>
2023-08-18 11:53:10 +0700

    implemented simple frontend infrastructure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField';
+
+const App = () => {
+    return (
+        <div>
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+        </div>
+    )
+}
+
+export default App;
