06246ae79d0470a7537d30f343d5c2ec2efe0f59
Aleksandr Vasilev <ap.vasilyev@yandex.ru>
2023-08-18 18:57:53 +0300

    feat: add cors-config for frontend (#5)
    Co-authored-by: Aleksandr Vasilev <a.vasilev@hh.ru>

---
+++ backend/src/main/java/ru/hh/summarizer/config/CorsFilterConfig.java
@@ -0,0 +1,29 @@
+package ru.hh.summarizer.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsFilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("http://frontend:5099");
+        config.addAllowedOrigin("/**");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
